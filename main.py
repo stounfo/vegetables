86b1c37bb40d0bccaccc4d7ba4680cac911d8a5a
Oleg Levantin <od@datalab.fit>
2019-09-08 21:35:03 +0300

    Add transformation to json in get_kingdoms method

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from aiopg.sa import create_engine
 import sqlalchemy as sa
 from orm_tables import kingdoms, products, categories
 from aiohttp import web
+import json
 
 async def _create_connection():
     engine = await create_engine(
@@ -17,18 +18,19 @@ async def _create_connection():
 
 async def get_kingdoms(request):
     list_of_kingdoms = list()
-
+    
     query = sa.select([kingdoms])
     async for row in conn.execute(query):
         list_of_kingdoms.append({"kingdom_id": row.kingdom_id, "name": row.name})
 
-    return web.Response(text=str(list_of_kingdoms))
-    
+    return web.Response(text=str(json.dumps(list_of_kingdoms, ensure_ascii=False)))
+
 
 
 if __name__ == "__main__":
     app = web.Application()
     conn = asyncio.get_event_loop().run_until_complete(_create_connection())
+
     app.add_routes([web.get('/get_kingdoms', get_kingdoms)])
 
     web.run_app(app, host='0.0.0.0', port=8081)
