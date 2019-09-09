4b5e9fb41bbae3920b6176b8543ab7203282c351
Oleg Levantin <od@datalab.fit>
2019-09-09 14:12:28 +0300

    Update fill_tables

--- fill_tables.sql
+++ fill_tables.sql
@@ -9,6 +9,6 @@ INSERT INTO "categories"(category_id, name, kingdom_id) VALUES (3, 'Лук', 3);
 INSERT INTO "categories"(category_id, name, kingdom_id) VALUES (4, 'Огурцы', 3);
 
 INSERT INTO "products"(product_id, name, cost, category_id, photo) VALUES (1, 'Шампиньоны 1', 25, 1, '/photo/1');
-INSERT INTO "products"(product_id, name, cost, category_id, photo) VALUES (2, 'Баклажан 1 ', 25, 2, '/photo/1');
-INSERT INTO "products"(product_id, name, cost, category_id, photo) VALUES (3, 'Лук 1', 25, 3, '/photo/1');
-INSERT INTO "products"(product_id, name, cost, category_id, photo) VALUES (4, 'Лук 2', 25, 3, '/photo/1');
+INSERT INTO "products"(product_id, name, cost, category_id, photo) VALUES (2, 'Баклажан 1 ', 25, 2, '/photo/2');
+INSERT INTO "products"(product_id, name, cost, category_id, photo) VALUES (3, 'Лук 1', 25, 3, '/photo/3');
+INSERT INTO "products"(product_id, name, cost, category_id, photo) VALUES (4, 'Лук 2', 25, 3, '/photo/4');
