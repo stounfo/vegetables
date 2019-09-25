2daf9b4639d1f01d836c6fed626fadfb4f3d54b9
Oleg Levantin <od@datalab.fit>
2019-09-25 17:01:07 +0300

    Rename col in users in db and orm

--- orm_tables.py
+++ orm_tables.py
@@ -20,8 +20,8 @@ products = sa.Table("products", sa.MetaData(),
 
 
 users = sa.Table("users", sa.MetaData(),
-                    sa.Column("id", sa.Integer, primary_key=True),
-                    sa.Column("user_id", sa.String),
+                    sa.Column("user_id", sa.Integer, primary_key=True),
+                    sa.Column("user", sa.String),
                     sa.Column("client_type", sa.String),
                     sa.Column("name", sa.String),
                     sa.Column("phone", sa.String),
