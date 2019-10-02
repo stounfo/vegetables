c8d62bc2a2f58a8b0b0c128ca2edc6810896a908
Oleg Levantin <od@datalab.fit>
2019-10-02 22:34:50 +0300

    Reengineering delete_item_from_cart

--- main.py
+++ main.py
@@ -5,6 +5,20 @@ import json
 
 routes = web.RouteTableDef()
 
+@routes.post('/add_user')
+async def add_user(request):
+    request_data = await request.json()
+    user_code = str(request_data["user_code"])
+    client_type = request_data["client_type"]
+
+    user_id = await database.user_exists(user_code, client_type)
+    if user_id:
+        return web.json_response({"result": "error"})
+    else:
+        user_id = await database.insert_into_users(user_code, client_type)
+        await database.insert_into_carts(user_id)
+        return web.json_response({"result": "success"})
+
 
 @routes.get('/get_categories')
 async def get_categories(request):
@@ -24,21 +38,6 @@ async def get_products(request):
     subcategory_id = await request.json()    
     products = await database.select_products(subcategory_id["subcategory_id"])
     return web.json_response(products)
-
-
-@routes.post('/add_user')
-async def add_user(request):
-    request_data = await request.json()
-    user_code = str(request_data["user_code"])
-    client_type = request_data["client_type"]
-
-    user_id = await database.user_exists(user_code, client_type)
-    if user_id:
-        return web.json_response({"result": "error"})
-    else:
-        user_id = await database.insert_into_users(user_code, client_type)
-        await database.insert_into_carts(user_id)
-        return web.json_response({"result": "success"})
 
 
 @routes.post('/add_item_to_cart')
@@ -80,10 +79,14 @@ async def get_cart_items(request):
 @routes.post('/delete_item_from_cart')
 async def delete_item_from_cart(request):
     request_data = await request.json()
-    cart_id = request_data["cart_id"]
     product_id = request_data["product_id"]
+    user_code = str(request_data["user_code"])
+    client_type = request_data["client_type"]
+
+    user_id = await database.user_exists(user_code, client_type)
+    cart_id = await database.get_cart_id(user_id)
     await database.delete_item_from_cart(cart_id, product_id)
-    return web.Response(text="success")
+    return web.json_response({"result": "success"})
 
 
 @routes.post('/get_user_info')
